f8b9d35f5160771cff8779895ecac2e0dae2130b
Aaloklowanshi <lowanshiaalok@gmail.com>
2023-10-10 23:18:56 +0530

    defined dirname commit

--- server.js
+++ server.js
@@ -9,12 +9,13 @@ import categoryRoutes from "./routes/categoryRoutes.js"
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from 'path';
-
+import { fileURLToPath } from "url";
 const app = express();
 
 //database config
 connectdb();
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 dotenv.config();
 app.use(express.json());
 app.use(morgan('dev'));
